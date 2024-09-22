d3c2776ffc7833a6fcfc3496197b510d0834a024
Shivam Dubey <134126380+shivamd332@users.noreply.github.com>
2024-09-23 00:58:29 +0530

    Add files via upload

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,101 @@
+import {Form} from "react-router-dom"
+
+const CreatePost = () => {
+  // const { addPost } = useContext(PostList);
+
+  return (
+    <Form method="POST" className="create-post" >
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Enter your User Id here
+        </label>
+        <input
+          type="text"
+          name="userId"
+          className="form-control"
+          id="userId"
+          placeholder="Your User Id"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          className="form-control"
+          id="title"
+          placeholder="How are you felling today"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          rows="4"
+          name="body"
+          className="form-control"
+          id="body"
+          placeholder="Tell us more about it"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          type="text"
+          name="reactions"
+          className="form-control"
+          id="reactions"
+          placeholder="How many people reacted to this post"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags here
+        </label>
+        <input
+          type="text"
+          name="tags"
+          className="form-control"
+          id="tags"
+          placeholder="Please enter tags using space"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </Form>
+  );
+};
+
+
+export const createPostAction = () => {
+  console.log(fetch("https://dummyjson.com/posts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: postTitle,
+      body: postBody,
+      reactions: reactions,
+      userId: userId,
+      tags: tags,
+    }),
+  }))
+    .then((res) => res.json())
+    .then((post) => {
+      addPost(post);
+      navigate("/");
+    });
+};
+
+
+export default CreatePost;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./routes/App.jsx";
+import CreatePost, { createPostAction } from "./components/CreatePost.jsx";
+import PostList, { postLoader } from "./components/PostList.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <PostList />, loader: postLoader },
+      {
+        path: "/create-post",
+        element: <CreatePost />,
+        action: createPostAction,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
